e271bf514ba18999914c3a25e980421b3c9d98d6
main <49618395+ZukiGA@users.noreply.github.com>
2021-04-28 15:53:36 -0500

    bug fixed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
+// incluimos cors
+const cors = require('cors');
+//incluimos una instancia de express server
 const express = require('express');
-
+//incluimos archivo de rutas
 const routes = require('../routes');
-
+//definimos la instancia del server
 const server = express();
 
-server.use(express.json());
- 
-server.use('/api',routes);
+// usamos cors en nuestro server
+server.use(cors());
+server.options('*', cors());
 
+server.use(express.json());
+
+server.use('/api', routes);
+
+// se exporta objeto server para poder ser usado
 module.exports = server;
